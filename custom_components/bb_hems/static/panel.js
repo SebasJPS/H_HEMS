fcc47bc211252077744d3b0b4682897f31cb1201
sebastian-bennecke <sebastian.bennecke@bossinfo.com>
2026-05-27 15:57:25 +0200

    fix: align panel version with v0.1.19

--- custom_components/bb_hems/static/panel.js
+++ custom_components/bb_hems/static/panel.js
@@ -15,7 +15,7 @@ const ALIASES = {
   flexible_loads_allowed: ["flexible_loads_allowed", "flexible verbraucher erlaubt"],
 };
 
-const BB_HEMS_VERSION = "0.1.18";
+const BB_HEMS_VERSION = "0.1.19";
 
 class BbHemsPanel extends HTMLElement {
   set hass(hass) {
